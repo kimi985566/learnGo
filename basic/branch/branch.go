96339503c20502fe7960df3e0aab2cae31480d86
kimi985566 <chengyu_yang@outlook.com>
2018-02-27 20:57:44 +0800

    -branch

--- basic/branch/branch.go
+++ basic/branch/branch.go
@@ -5,12 +5,36 @@ import (
 	"fmt"
 )
 
+func grade(score int) string {
+	g := ""
+	switch {
+	case score < 0 || score > 100:
+		panic(fmt.Sprintf("Wrong Score: %d", score))
+	case score < 60:
+		g = "F"
+	case score < 80:
+		g = "C"
+	case score < 90:
+		g = "B"
+	case score <= 100:
+		g = "A"
+	}
+	return g
+}
+
 func main() {
 	const file = "abc.txt"
-	contents, err := ioutil.ReadFile(file)
-	if err != nil {
+	if contents, err := ioutil.ReadFile(file); err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Printf("%s\n", contents)
 	}
+
+	fmt.Println(
+		grade(59),
+		grade(88),
+		grade(99),
+		grade(100),
+		grade(0),
+	)
 }
